802e7c88ce30b9520c3b24a8561d0610072fe52b
Pasin Suriyentrakorn <pasin@couchbase.com>
2017-02-18 13:32:52 -0800

    Implement Swift Subdocument
    * Implemented Swift Subdocument by wrapping CBLSubdocument.
    * Swift Subdocument is weakly cached inside CBLSubdocument. When getting the Swift Subdocument, trying to get it from the CBLSubdocument impl first before creating it.

--- Swift/Properties.swift
+++ Swift/Properties.swift
@@ -12,25 +12,22 @@ import Foundation
 public class Properties {
 
     public var properties : [String:Any]? {
-        get {return _impl.properties}
-        set {_impl.properties = newValue}
+        get {return convertGetterValue(_impl.properties) as? [String: Any]}
+        set {_impl.properties = convertSetterValue(newValue) as? [String : Any]}
     }
 
-
     public func property(_ key: String) -> Any? {
-        return _impl.object(forKey: key)
+        return convertGetterValue(_impl.object(forKey: key))
     }
 
     public func setProperty(_ key: String, _ value: Any?) {
-        return _impl.setObject(value, forKey: key)
+        return _impl.setObject(convertSetterValue(value), forKey: key)
     }
-
 
     public func contains(_ key: String) -> Bool {
         return _impl.containsObject(forKey: key)
     }
 
-    
     public subscript(key: String) -> Bool {
         get {return _impl.boolean(forKey: key)}
         set {_impl.setBoolean(newValue, forKey: key)}
@@ -66,22 +63,58 @@ public class Properties {
     public subscript(key: String) -> [Any]? {
         get {return _impl.object(forKey: key) as? [Any]}
     }
-
-    public subscript(key: String) -> CBLSubdocument? {
-        get {return _impl.object(forKey: key) as? CBLSubdocument}
+    
+    public subscript(key: String) -> Subdocument? {
+        get {return convertGetterValue(_impl.object(forKey: key)) as? Subdocument}
     }
-
+    
     public subscript(key: String) -> Any? {
-        get {return _impl.object(forKey: key)}
-        set {_impl.setObject(newValue, forKey: key)}
+        get {return property(key)}
+        set {setProperty(key, newValue)}
     }
-
 
     init(_ impl: CBLProperties) {
         _impl = impl
     }
 
     let _impl: CBLProperties
+    
+    func convertGetterValue(_ value: Any?) -> Any? {
+        switch value {
+        case let implSubdoc as CBLSubdocument:
+            if let subdoc = implSubdoc.swiftSubdocument {
+                return subdoc
+            }
+            return Subdocument(implSubdoc)
+        case let array as [Any]:
+            var result: [Any] = [];
+            for v in array { result.append(convertGetterValue(v)!) }
+            return result
+        case let dict as [String: Any]:
+            var result: [String: Any] = [:]
+            for (k, v) in dict { result[k] = convertGetterValue(v)! }
+            return result
+        default:
+            return value
+        }
+    }
+    
+    func convertSetterValue(_ value: Any?) -> Any? {
+        switch value {
+        case let subdoc as Subdocument:
+            return subdoc._subdocimpl
+        case let array as [Any]:
+            var result: [Any] = [];
+            for v in array { result.append(convertSetterValue(v)!) }
+            return result
+        case let dict as [String: Any]:
+            var result: [String: Any] = [:]
+            for (k, v) in dict { result[k] = convertSetterValue(v)! }
+            return result
+        default:
+            return value
+        }
+    }
 }
 
 
